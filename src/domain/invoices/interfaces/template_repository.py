b4e4f3c1b354504b0032a3607ae91a39e82a5ec3
juanj72 <juan.jara@campusucc.edu.co>
2025-05-16 10:30:57 -0500

    feat: folder reestructure to clean architecture

---
+++ src/domain/invoices/interfaces/template_repository.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+
+class TemplateRepository(ABC):
+    @abstractmethod
+    def save(self, template_name: str, content: str) -> None:
+        # almacena/reemplaza el template en el sistema
+        pass
+
+    @abstractmethod
+    def load(self, template_name: str) -> str:
+        # devuelve el template
+        pass
+
+    @abstractmethod
+    def list(self) -> Iterable[str]:
+        # devuelve la lista de templates
+        pass
